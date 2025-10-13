1d4847fb457f508aaeb5c3482d24b62f19cabc2f
SantiagoPaje <santipaje@hotmail.com>
2025-10-13 20:56:44 +0200

    Implements the infrastructure configuration

---
+++ src/AuthService.Infrastructure/Persistence/AuthDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AuthService.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Infrastructure.Persistence
+{
+    /// <summary>
+    /// AuthDb context which extends IdentityDbContext
+    /// </summary>
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Keeps clean entities and separate config
+            builder.ApplyConfigurationsFromAssembly(typeof(AuthDbContext).Assembly);
+        }
+
+    }
+}
---
+++ src/AuthService.Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using AuthService.Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Infrastructure.Persistence.Configurations
+{
+    /// <summary>
+    /// ApplicationUser configuration
+    /// </summary>
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(u => u.UserName)
+                .HasMaxLength(100);
+
+            builder.Property(u => u.CreatedTime)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+        }
+    }
+}
