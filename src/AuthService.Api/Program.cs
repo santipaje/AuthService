0b191a5bf8f7280b04c58e4d97249d12a2bf0f6f
SantiagoPaje <santipaje@hotmail.com>
2025-10-20 02:37:39 +0200

    Includes database configuration and initial migrations

--- src/AuthService.Api/Program.cs
+++ src/AuthService.Api/Program.cs
@@ -1,16 +1,68 @@
+using AuthService.Infrastructure.Identity;
+using AuthService.Infrastructure.Identity.Seed;
 using AuthService.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Logging configuration
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Infrastrucute and DB configuration
 builder.Services.AddInfrastructure(builder.Configuration);
 
+// TODO:
+// Adds applications services
+//builder.Services.AddApplicationServices();
+
+// Controllers
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// App Builder
 var app = builder.Build();
+
+// Migrations and data seeds
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    // Gets logger factory
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    var logger = loggerFactory.CreateLogger("Seeder");
+
+    if (app.Environment.IsDevelopment())
+    {
+        try
+        {
+            // Automatic database migration
+            var context = services.GetRequiredService<AuthDbContext>();
+            await context.Database.MigrateAsync();
+
+            // Identity managers
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            // Execute seeders
+            await DefaultRolesSeeder.SeedAsync(roleManager, logger);
+            await DefaultAdminSeeder.SeedAsync(userManager, logger);
+
+            logger.LogInformation("Database migrated and seeded successfully.");
+
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error while migrating or seeding the database.");
+            throw;
+        }
+    }
+
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,6 +73,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Enables authentication and authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ src/AuthService.Infrastructure/Identity/Seed/DefaultRolesSeeder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Infrastructure.Identity.Seed
+{
+    /// <summary>
+    /// Default seeder to create the indicated roles if they do not exist.
+    /// </summary>
+    public static class DefaultRolesSeeder
+    {
+        private static readonly string[] Roles = ["Admin", "User"];
+
+        /// <summary>
+        /// Creates the roles if they do not exist.
+        /// </summary>
+        /// <param name="roleManager"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        public static async Task SeedAsync(RoleManager<IdentityRole> roleManager, ILogger logger)
+        {
+            foreach (var role in Roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                    logger.LogInformation("Role '{rolen}' created successfully", role);
+                }
+            }
+        }
+
+    }
+}
