1059dd6db5d75bb6aebf487ad97e8cd0f113f460
santipaje <santipaje@hotmail.com>
2025-11-05 00:06:56 +0100

    Implements Token Service

---
+++ src/AuthService.Application/DTOs/UserInfoDTO.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AuthService.Application.DTOs
+{
+    /// <summary>
+    /// User info DTO
+    /// </summary>
+    public class UserInfoDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public IList<string> Roles { get; set; } = [];
+    }
+}
---
+++ src/AuthService.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using AuthService.Application.DTOs;
+namespace AuthService.Application.Interfaces
+{
+    /// <summary>
+    /// Token Service Interface
+    /// </summary>
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Generates a token with the user and roles
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="roles"></param>
+        /// <returns></returns>
+        string GenerateToken(UserInfoDTO user, IList<string> roles);
+    }
+}
