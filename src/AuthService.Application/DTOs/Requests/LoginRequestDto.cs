86e939a24dcb7f7a990e97bcc9a094c33e5b8586
santipaje <santipaje@hotmail.com>
2025-11-17 03:07:41 +0100

    Creates necessary DTOs for login and register

---
+++ src/AuthService.Application/DTOs/Requests/LoginRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.Application.DTOs.Requests
+{
+    /// <summary>
+    /// Login DTO
+    /// </summary>
+    public class LoginRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
