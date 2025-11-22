4db1cbb40c5900c6dfdb374b42cad9843a18ee67
santipaje <santipaje@hotmail.com>
2025-11-22 21:53:08 +0100

    Implements Validators unit tests

---
+++ tests/AuthService.UnitTests/Validators/LoginRequestValidatorTest.cs
@@ -0,0 +1,55 @@
+﻿using AuthService.Application.DTOs.Requests;
+using AuthService.Application.Validators;
+using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace AuthService.UnitTests.Validators
+{
+    public class LoginRequestValidatorTest
+    {
+        private readonly LoginRequestValidator _validator;
+
+        public LoginRequestValidatorTest()
+        {
+            _validator = new LoginRequestValidator();
+        }
+
+        // Email
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Empty()
+        {
+            var model = new LoginRequestDto { Email = string.Empty, Password = "Password1!"};
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Invalid()
+        {
+            var model = new LoginRequestDto { Email = "not-an-email", Password = "Password1!" };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        // Password
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Is_Empty()
+        {
+            var model = new LoginRequestDto { Email = "user@test.com", Password = string.Empty };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password);
+        }
+
+        // Correct model
+
+        [Fact]
+        public void Should_Not_Have_Errors_When_Request_Is_Valid()
+        {
+            var model = new LoginRequestDto { Email = "user@test.com", Password = "Password1!" };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
---
+++ tests/AuthService.UnitTests/Validators/RegisterRequestValidatorTests.cs
@@ -0,0 +1,111 @@
+﻿
+using AuthService.Application.Common.Constants;
+using AuthService.Application.DTOs.Requests;
+using AuthService.Application.Validators;
+using FluentValidation.Results;
+using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace AuthService.UnitTests.Validators
+{
+    /// <summary>
+    /// Testing class for the Register Request Validator
+    /// </summary>
+    public class RegisterRequestValidatorTests
+    {
+        private readonly RegisterRequestValidator _validator;
+
+        public RegisterRequestValidatorTests()
+        {
+            _validator = new RegisterRequestValidator();
+        }
+
+        // Email
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Empty()
+        {
+            var model = new RegisterRequestDto { Email = string.Empty, FullName = "test-full-name", Password = "Password1123!", UserName = "test-user-name" };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Email_Is_Invalid()
+        {
+            var model = new RegisterRequestDto { Email = "not-an-email", FullName = "test-full-name", Password = "Password1123!", UserName = "test-user-name" };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        // FullName
+
+        [Fact]
+        public void Should_Have_Error_When_FullName_Is_Empty()
+        {
+            var model = new RegisterRequestDto { Email = "user@test.com", FullName = string.Empty, Password = "Password1123!", UserName = "test-user-name" };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.FullName);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_FullName_Is_Too_Short()
+        {
+            var model = new RegisterRequestDto { Email = "user@test.com", FullName = "a", Password = "Password1123!", UserName = "test-user-name" };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.FullName);
+        }
+
+        // UserName
+
+        [Fact]
+        public void Should_Have_Error_When_UserName_Is_Empty()
+        {
+            var model = new RegisterRequestDto { Email = "user@test.com", FullName = "test-full-name", Password = "Password1123!", UserName = string.Empty };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.UserName);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_UserName_Is_Too_Short()
+        {
+            var model = new RegisterRequestDto { Email = "user@test.com", FullName = "a", Password = "Password1123!", UserName = "a" };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.UserName);
+        }
+
+        // Password
+
+        private TestValidationResult<RegisterRequestDto> ValidatePassword(string password)
+        {
+            var model = new RegisterRequestDto { Email = "user@test.com", FullName = "test-full-name", Password = password, UserName = "test-user-name" };
+            return _validator.TestValidate(model);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("Pass1234!", "")]
+        [InlineData("password1234!", ValidatorMessages.PasswordRequiresUppercase)]
+        [InlineData("PASSWORD1234!", ValidatorMessages.PasswordRequiresLowercase)]
+        [InlineData("Password!!!!", ValidatorMessages.PasswordRequiresDigit)]
+        [InlineData("Password1234", ValidatorMessages.PasswordRequiresNonAlphanumeric)]
+        public void Password_Should_Return_Specific_Error(string password, string expectedMessage)
+        {
+            var result = ValidatePassword(password);
+
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                  .WithErrorMessage(expectedMessage);
+        }
+
+
+        // Correct model
+
+        [Fact]
+        public void Should_Not_Have_Errors_When_Request_Is_Valid()
+        {
+            var model = new RegisterRequestDto { Email = "user@test.com", FullName = "test-full-name", Password = "Password1123!", UserName = "test-user-name" };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
